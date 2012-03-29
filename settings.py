56ca34ab735dee95be94c73276e9e75b884735de
Anatoliy Guskov <anatoliy.guskov@gmail.com>
2012-03-29 21:26:15 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -20,7 +20,7 @@ DATABASES = {
         'ENGINE': 'mysql',               # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'virtmgr',               # Or path to database file if using sqlite3.
         'USER': 'django',                # Not used with sqlite3.
-        'PASSWORD': '1qAz45G!c',         # Not used with sqlite3.
+        'PASSWORD': '',         # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -115,5 +115,5 @@ EMAIL_PORT = 25
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
-DEFAULT_FROM_EMAIL = 'Web Virtual Manager Machine <noreply@x-sys.com.ua>'
+DEFAULT_FROM_EMAIL = ''
 
