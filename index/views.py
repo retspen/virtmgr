95512000c70a7bc2902cefad872220e20554d39c
Anatoliy Guskov <anatoliy.guskov@gmail.com>
2012-04-01 12:00:51 +0300

    Del all .pyc & userdata, index

--- index/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render_to_response
-from django.http import Http404, HttpResponse, HttpResponseRedirect
-
-def index(request):
-    return render_to_response('index.html', locals())
