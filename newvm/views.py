709d1e94a91fef56f11ffa1dad4a7c7106ed579e
Anatoliy Guskov <anatoliy.guskov@gmail.com>
2012-04-03 11:37:57 +0300

    Fix newvm

--- newvm/views.py
+++ newvm/views.py
@@ -166,12 +166,12 @@ def index(request, host):
 				  <on_crash>restart</on_crash>
 				  <devices>""" % (name, mem, memaloc, cpus, arch, machine)
 			
-			if arch == 'x86_64':
-				xml += """<emulator>%s</emulator>""" % (emul[1])
-			else:
-				xml += """<emulator>%s</emulator>""" % (emul[0])
+		if arch == 'x86_64':
+			xml += """<emulator>%s</emulator>""" % (emul[1])
+		else:
+			xml += """<emulator>%s</emulator>""" % (emul[0])
 
-			xml += """<disk type='file' device='disk'>
+		xml += """<disk type='file' device='disk'>
 				      <driver name='qemu' type='%s'/>
 				      <source file='%s'/>
 				      <target dev='hda' bus='ide'/>
@@ -189,14 +189,14 @@ def index(request, host):
 				    </controller>
 				    """ % (img_frmt, img, iso)
 
-			if re.findall("br", bridge):
-				xml += """<interface type='bridge'>
-						<source bridge='%s'/>""" % (bridge)
-			else:
-				xml += """<interface type='network'>
+		if re.findall("br", bridge):
+			xml += """<interface type='bridge'>
+					<source bridge='%s'/>""" % (bridge)
+		else:
+			xml += """<interface type='network'>
 					<source network='%s'/>""" % (bridge)
 			
-			xml += """<address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x0'/>
+		xml += """<address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x0'/>
 				    </interface>
 				    <input type='tablet' bus='usb'/>
 				    <input type='mouse' bus='ps2'/>
