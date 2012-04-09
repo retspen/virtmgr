2591cdbc5f8ffc8beff4b4f28fba1049441f01d6
Anatoliy Guskov <anatoliy.guskov@gmail.com>
2012-04-09 11:34:44 +0300

    Add dashboard form

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,17 +3,14 @@ import socket
 from django.shortcuts import render_to_response
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from virtmgr.model.models import *
+from virtmgr.dashboard.forms import add_host
 
 def index(request):
 	if not request.user.is_authenticated():
 		return HttpResponseRedirect('/user/login')
 
-	usr_id = request.user.id
-	usr_name = request.user
-	
 	def get_hosts_status():
-		usr_id = request.user.id
-		kvm_host = Host.objects.filter(user=usr_id)
+		kvm_host = Host.objects.filter(user=request.user.id)
 		name_ipddr = {}
 		for host in kvm_host:
 			try:
@@ -28,17 +25,12 @@ def index(request):
 	   	return name_ipddr
 	
 	def del_host(host):
-		usr_id = request.user.id
-		hosts = Host.objects.get(user=usr_id, hostname=host)
+		hosts = Host.objects.get(user=request.user.id, hostname=host)
 		hosts.delete()
 
 	def add_host(host, ip, usr, passw):
-		hostname = Host.objects.filter(user=usr_id, hostname=host)
-		ipaddr = Host.objects.filter(user=usr_id, ipaddr=ip)
-		if len(hostname) == 0 and len(ipaddr) == 0:
-			if len(host) != 0 and len(ip) != 0 and len(usr) != 0 and len(passw) != 0:
-				hosts = Host(user_id=usr_id, hostname=host, ipaddr=ip, login=usr, passwd=passw)
-				hosts.save()
+		hosts = Host(user_id=request.user.id, hostname=host, ipaddr=ip, login=usr, passwd=passw)
+		hosts.save()
 
 	def get_host_status(hosts):
 		for host, info in hosts.items():
@@ -53,10 +45,14 @@ def index(request):
 			del_host(host)
 			return HttpResponseRedirect('/dashboard/')
 		if action == 'add':
-			name = request.POST.get('name','')
-			ipaddr = request.POST.get('ipaddr','')
-			login = request.POST.get('sshusr','')
-			passw = request.POST.get('passw','')
-			add_host(name, ipaddr, login, passw)
-			return HttpResponseRedirect('/dashboard/')
+			form = add_host(request.POST)
+			if form.is_valid():
+				field = form.cleaned_data
+				name = field['host']
+				ipaddr = field['ipaddr']
+				login = field['sshusr']
+				passw = field['passw']
+				add_host(name, ipaddr, login, passw)
+				return HttpResponseRedirect('/dashboard/')
+
 	return render_to_response('dashboard.html', locals())
